dc0f7d21b95e23482f27724e8350be162cd349be
blzhw <79049491+blzhw@users.noreply.github.com>
2024-03-10 01:49:21 +0800

    Update arm-magic.sh

--- scripts/arm-magic.sh
+++ scripts/arm-magic.sh
@@ -1,42 +1,191 @@
-#!/bin/sh
-#
-#   Empty/wrong machtype-workaround generator
-#
-#   Copyright (C) 2006-2012 Imre Kaloz <kaloz@openwrt.org>
-#   based on linux/arch/arm/boot/compressed/head-xscale.S
-#
-#   This program is free software; you can redistribute it and/or modify
-#   it under the terms of the GNU General Public License as published by
-#   the Free Software Foundation; either version 2 of the License, or
-#   (at your option) any later version.
-#
-#   This program is distributed in the hope that it will be useful,
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#   GNU General Public License for more details.
-#
-#   You should have received a copy of the GNU General Public License
-#   along with this program; if not, write to the Free Software
-#   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#!/bin/bash
 
-# NOTE: for now it's for only IXP4xx in big endian mode
+echo "========================= begin $0 ==========================="
+source make.env
+source public_funcs
+init_work_env
 
-# list of supported boards, in "boardname machtypeid" format
-for board in "avila 526" "gateway7001 731" "nslu2 597" "nas100d 865" "wg302v1 889" "wg302v2 890" "pronghorn 928" "pronghornmetro 1040" "compex 1273" "wrt300nv2 1077" "loft 849" "dsmg600 964" "fsg3 1091" "ap1000 1543" "tw2662 1658" "tw5334 1664" "ixdpg425 604" "cambria 1468" "sidewinder 1041" "ap42x 4418"
-do
-  set -- $board
-  hexid=$(printf %x\\n $2)
-  if [ "$2" -lt "256" ]; then
-    # we have a low machtypeid, we just need a "mov" (e3a)
-    printf "\xe3\xa0\x10\x$hexid" > $BIN_DIR/$IMG_PREFIX-$1-zImage
-  else
-    # we have a high machtypeid, we need a "mov" (e3a) and an "orr" (e38)
-    if [ "$2" -lt "4096" ]; then
-      printf "\xe3\xa0\x10\x$(echo $hexid|cut -b "2 3")\xe3\x81\x1c\x$(echo $hexid|cut -b 1)" > $BIN_DIR/$IMG_PREFIX-$1-zImage
-    else
-      printf "\xe3\xa0\x10\x$(echo $hexid|cut -b "3 4")\xe3\x81\x1c\x$(echo $hexid|cut -b "1 2")" > $BIN_DIR/$IMG_PREFIX-$1-zImage
-    fi
-  fi
-    # generate the image
-    cat $BIN_DIR/$IMG_PREFIX-zImage >> $BIN_DIR/$IMG_PREFIX-$1-zImage
-done
+# 盒子型号识别参数 
+PLATFORM=amlogic
+SOC=s905
+BOARD=mxq-pro+
+
+SUBVER=$1
+
+# Kernel image sources
+###################################################################
+KERNEL_TAGS="stable"
+KERNEL_BRANCHES="mainline:all:>=:5.4"
+MODULES_TGZ=${KERNEL_PKG_HOME}/modules-${KERNEL_VERSION}.tar.gz
+check_file ${MODULES_TGZ}
+BOOT_TGZ=${KERNEL_PKG_HOME}/boot-${KERNEL_VERSION}.tar.gz
+check_file ${BOOT_TGZ}
+DTBS_TGZ=${KERNEL_PKG_HOME}/dtb-amlogic-${KERNEL_VERSION}.tar.gz
+check_file ${DTBS_TGZ}
+K510=$(get_k510_from_boot_tgz "${BOOT_TGZ}" "vmlinuz-${KERNEL_VERSION}")
+export K510
+###########################################################################
+
+# Openwrt root 源文件
+OPWRT_ROOTFS_GZ=$(get_openwrt_rootfs_archive ${PWD})
+check_file ${OPWRT_ROOTFS_GZ}
+echo "Use $OPWRT_ROOTFS_GZ as openwrt rootfs!"
+
+# 目标镜像文件
+TGT_IMG="${WORK_DIR}/openwrt_${SOC}_${BOARD}_${OPENWRT_VER}_k${KERNEL_VERSION}${SUBVER}.img"
+
+# 补丁和脚本
+###########################################################################
+KMOD="${PWD}/files/kmod"
+KMOD_BLACKLIST="${PWD}/files/kmod_blacklist"
+MAC_SCRIPT1="${PWD}/files/fix_wifi_macaddr.sh"
+MAC_SCRIPT2="${PWD}/files/find_macaddr.pl"
+MAC_SCRIPT3="${PWD}/files/inc_macaddr.pl"
+CPUSTAT_SCRIPT="${PWD}/files/cpustat"
+CPUSTAT_SCRIPT_PY="${PWD}/files/cpustat.py"
+INDEX_PATCH_HOME="${PWD}/files/index.html.patches"
+GETCPU_SCRIPT="${PWD}/files/getcpu"
+TTYD="${PWD}/files/ttyd"
+FLIPPY="${PWD}/files/scripts_deprecated/flippy_cn"
+BANNER="${PWD}/files/banner"
+
+# 20200314 add
+FMW_HOME="${PWD}/files/firmware"
+SMB4_PATCH="${PWD}/files/smb4.11_enable_smb1.patch"
+SYSCTL_CUSTOM_CONF="${PWD}/files/99-custom.conf"
+
+# 20200709 add
+COREMARK="${PWD}/files/coremark.sh"
+
+# 20200930 add
+# 经实测，MXQ Pro+不能写入emmc，写入必砖！
+#INST_SCRIPT="${PWD}/files/s905/install-to-emmc.sh"
+#UPDATE_SCRIPT="${PWD}/files/s905/update-to-emmc.sh"
+SND_MOD="${PWD}/files/s905/snd-meson-gx"
+DAEMON_JSON="${PWD}/files/s905/daemon.json"
+
+# 20201006 add
+FORCE_REBOOT="${PWD}/files/s905/reboot"
+# 20201017 add
+BAL_ETH_IRQ="${PWD}/files/balethirq.pl"
+# 20201026 add
+FIX_CPU_FREQ="${PWD}/files/fixcpufreq.pl"
+SYSFIXTIME_PATCH="${PWD}/files/sysfixtime.patch"
+
+# 20201128 add
+SSL_CNF_PATCH="${PWD}/files/openssl_engine.patch"
+
+# 20201212 add
+BAL_CONFIG="${PWD}/files/s905/balance_irq"
+CPUFREQ_INIT="${PWD}/files/s905/cpufreq"
+
+# 20210302 modify
+FIP_HOME="${PWD}/files/meson_btld/with_fip/s905"
+UBOOT_WITH_FIP="${FIP_HOME}/n1-u-p201.bin.sd.bin"
+UBOOT_WITHOUT_FIP_HOME="${PWD}/files/meson_btld/without_fip"
+UBOOT_WITHOUT_FIP="u-boot-p201.bin"
+
+# 20210208 add
+WIRELESS_CONFIG="${PWD}/files/s905/wireless"
+
+# 20210307 add
+SS_LIB="${PWD}/files/ss-glibc/lib-glibc.tar.xz"
+SS_BIN="${PWD}/files/ss-glibc/armv8a/ss-bin-glibc.tar.xz"
+JQ="${PWD}/files/jq"
+
+# 20210330 add
+DOCKERD_PATCH="${PWD}/files/dockerd.patch"
+
+# 20200416 add
+FIRMWARE_TXZ="${PWD}/files/firmware_armbian.tar.xz"
+BOOTFILES_HOME="${PWD}/files/bootfiles/amlogic"
+GET_RANDOM_MAC="${PWD}/files/get_random_mac.sh"
+
+# 20210618 add
+DOCKER_README="${PWD}/files/DockerReadme.pdf"
+
+# 20210704 add
+SYSINFO_SCRIPT="${PWD}/files/30-sysinfo.sh"
+
+# 20210923 add 
+OPENWRT_KERNEL="${PWD}/files/openwrt-kernel"
+OPENWRT_BACKUP="${PWD}/files/openwrt-backup"
+
+# 20211019 add
+FIRSTRUN_SCRIPT="${PWD}/files/first_run.sh"
+# 20211214 add
+P7ZIP="${PWD}/files/7z"
+# 20211217 add
+DDBR="${PWD}/files/openwrt-ddbr"
+# 20220225 add
+SSH_CIPHERS=""
+SSHD_CIPHERS=""
+###########################################################################
+
+check_depends
+
+SKIP_MB=4
+BOOT_MB=160
+ROOTFS_MB=736
+SIZE=$((SKIP_MB + BOOT_MB + ROOTFS_MB))
+create_image "$TGT_IMG" "$SIZE"
+create_partition "$TGT_DEV" "msdos" "$SKIP_MB" "$BOOT_MB" "fat32" "0" "-1" "btrfs"
+make_filesystem "$TGT_DEV" "B" "fat32" "BOOT" "R" "btrfs" "ROOTFS"
+mount_fs "${TGT_DEV}p1" "${TGT_BOOT}" "vfat"
+mount_fs "${TGT_DEV}p2" "${TGT_ROOT}" "btrfs" "compress=zstd:${ZSTD_LEVEL}"
+echo "创建 /etc 子卷 ..."
+btrfs subvolume create $TGT_ROOT/etc
+extract_rootfs_files
+extract_amlogic_boot_files
+
+echo "修改引导分区相关配置 ... "
+cd $TGT_BOOT
+rm -f uEnv.ini
+cat > uEnv.txt <<EOF
+LINUX=/zImage
+INITRD=/uInitrd
+
+# 下列 dtb，用到哪个就把哪个的#删除，其它的则加上 # 在行首
+
+# 用于 MXQ Pro+ (4K, 2G/16G, S905)
+FDT=/dtb/amlogic/meson-gxbb-mxq-pro-plus.dtb
+
+APPEND=root=UUID=${ROOTFS_UUID} rootfstype=btrfs rootflags=compress=zstd:${ZSTD_LEVEL} console=ttyAML0,115200n8 console=tty0 no_console_suspend consoleblank=0 fsck.fix=yes fsck.repair=yes net.ifnames=0 cgroup_enable=cpuset cgroup_memory=1 cgroup_enable=memory swapaccount=1
+EOF
+
+echo "uEnv.txt -->"
+echo "==============================================================================="
+cat uEnv.txt
+echo "==============================================================================="
+echo
+
+echo "修改根文件系统相关配置 ... "
+cd $TGT_ROOT
+copy_supplement_files
+extract_glibc_programs
+adjust_docker_config
+adjust_openssl_config
+adjust_qbittorrent_config
+adjust_getty_config
+adjust_samba_config
+adjust_nfs_config "mmcblk2p4"
+adjust_openssh_config
+adjust_openclash_config
+use_xrayplug_replace_v2rayplug
+create_fstab_config
+adjust_turboacc_config
+adjust_ntfs_config
+adjust_mosdns_config
+patch_admin_status_index_html
+adjust_kernel_env
+copy_uboot_to_fs
+write_release_info
+write_banner
+config_first_run
+create_snapshot "etc-000"
+write_uboot_to_disk
+clean_work_env
+mv ${TGT_IMG} ${OUTPUT_DIR} && sync
+echo "镜像已生成! 存放在 ${OUTPUT_DIR} 下面!"
+echo "========================== end $0 ================================"
+echo
